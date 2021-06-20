1007e519121b9a2ad735ee42c6bb8963a243cfe8
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-06-20 17:54:13 -0400

    making changes to guessTheNumber.py

--- chapter5/guessTheNumber.py
+++ chapter5/guessTheNumber.py
@@ -1,14 +1,19 @@
 import random
-
 number = random.randint(1, 100)
 numberOfGuesses = 0
 while(numberOfGuesses < 10):
+    print("Welcome to the guess the number game! You only have 10 tries so guess wisely")
     print("Guess a number between 1 and 100 \n")
     guess = input()
     guess = int(guess)
     if guess == number:
-        print(f"You guessed it! The number was {number}")
+        print(f"You guessed it! The number was {number} You took {numberOfGuesses} tries to guess the answer.")
         break
+    elif guess != number:
+        if guess < number:
+            print("That number is too low guess again")
+        else:
+            print("That number is too high guess again")
+    else:
+        print(f"Sorry you didn't guess the right number the answer was {number}.")
     numberOfGuesses += 1
-
-print(f"Sorry you didn't guess the right number the answer was {number}")
